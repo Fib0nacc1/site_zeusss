c146976c95a920df6ad5699ebd401c596b654960
Fib0nacc1 <rodionbryncov@gmail.com>
2017-03-23 16:49:54 +0200

    new

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('.scrollto').mPageScroll2id();
+});
+$(window).load(function () {
+	//$('.preloader_outer').delay(400).fadeIn('slow');
+	$('.preloader_outer').delay(2000).fadeOut('fast');
+});
